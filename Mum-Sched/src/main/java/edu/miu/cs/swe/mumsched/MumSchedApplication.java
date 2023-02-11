a5d380e644be1a77ac649ca2002a9ae903515b6b
selam <selamawit.habtemariam@miu.edu>
2023-02-10 22:19:44 -0600

    java file

---
+++ Mum-Sched/src/main/java/edu/miu/cs/swe/mumsched/MumSchedApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.swe.mumsched;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumSchedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MumSchedApplication.class, args);
+    }
+
+}
---
+++ Mum-Sched/src/test/java/edu/miu/cs/swe/mumsched/MumSchedApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.swe.mumsched;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MumSchedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
